f1af9b94f1405d0b87f9f8e7ae4c0d467ee5f85a
AY02 <alessioyang.02@gmail.com>
2023-08-29 18:17:57 +0200

    new

--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import telebot
 from chatgpt import *
+from functions import *
 
 load_dotenv()
 
@@ -13,8 +14,12 @@ bot = telebot.TeleBot(TELEGRAM_API_KEY)
 @bot.message_handler(commands=['ask_gpt'])
 def ask_gpt(message):
     request = message.text.split(' ', 1)[1]
+    log(f'{message.from_user.username}: {request}')
     #response = ask_chatgpt(request)
-    response = 'You are a broke person'
+    response = 'You are too poor to afford ChatGPT'
     bot.reply_to(message, response)
+    log(f'Chat GPT: {response}')
+
+
 
 bot.polling()
